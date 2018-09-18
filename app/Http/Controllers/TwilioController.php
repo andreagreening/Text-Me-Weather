938bfb52da7d3d15ec4fe6cae49eb265e7253c4b
Jake Och <ger.minationstudio@gmail.com>
2018-09-18 16:11:35 -0400

    Remove Redundant Controller

--- app/Http/Controllers/TwilioController.php
+++ app/Http/Controllers/TwilioController.php
@@ -16,8 +16,6 @@ class TwilioController extends Controller
      */
     public function __construct()
     {
-        $account_sid = 'ACeaba3e0d757c54dddf756514c2006400';
-        $auth_token = '1e52a0d9aceda2b86764d9410b3b344d';
-        $this->twilioClient = new Services_Twilio($account_sid, $auth_token);
+        $this->twilioClient = new Services_Twilio(env('TWILIO_ACCOUNT_SID'), env('TWILIO_AUTH_TOKEN'));
     }
 }
